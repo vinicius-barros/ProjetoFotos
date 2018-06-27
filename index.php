3db9eab601c23537658f7c470cdf4194e389777e
Vinicius Silvestre <viniciussilvestrebarros@gmail.com>
2018-06-26 23:25:36 -0300

    Carregando arquivos para repositorio

---
+++ index.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <!-- Favicon icon -->
+<!--  <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">-->
+
+  <!-- Bootstrap Core CSS -->
+  <link href="publico/css/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
+  <!-- Custom CSS -->
+  <link href="publico/css/lib/owl.carousel.min.css" rel="stylesheet">
+  <link href="publico/css/lib/owl.theme.default.min.css" rel="stylesheet">
+  <link href="publico/css/helper.css" rel="stylesheet">
+  <link href="publico/css/style.css" rel="stylesheet">
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:** -->
+  <!--[if lt IE 9]>
+  <script src="https:**oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+  <script src="https:**oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+  <title>Michelly Nathaly</title>
+</head>
+<body>
+<?php include_once "site/includes/_naveBar.php";?>
+
+
+
+
+<!-- All Jquery -->
+<script src="publico/js/lib/jquery/jquery.min.js"></script>
+<!-- Bootstrap tether Core JavaScript -->
+<script src="publico/js/lib/bootstrap/js/popper.min.js"></script>
+<script src="publico/js/lib/bootstrap/js/bootstrap.min.js"></script>
+<!-- slimscrollbar scrollbar JavaScript -->
+<script src="publico/js/jquery.slimscroll.js"></script>
+
+
+<!--stickey kit -->
+<script src="publico/js/lib/sticky-kit-master/dist/sticky-kit.min.js"></script>
+<!--Custom JavaScript -->
+
+</body>
+</html>
